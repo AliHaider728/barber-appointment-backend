8ed44951abe1d7e729f565de3b1624edd6782dcd
AliHaider728 <decantboy67@gmail.com>
2025-11-16 21:05:37 +0500

    "remove stripe"

--- models/Barber.js
+++ models/Barber.js
@@ -1,13 +1,16 @@
+// models/Barber.js
 import mongoose from 'mongoose';
 
 const barberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   experienceYears: { type: Number, required: true },
-  gender: { type: String, enum: ['male', 'female'], required: true },
+  gender: { 
+    type: String, 
+    enum: ['male', 'female'], 
+    required: true 
+  },
   specialties: { type: [String], required: true },
-  branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true },
-  stripeAccountId: { type: String },
-  isOnboarded: { type: Boolean, default: false }
+  branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', required: true }
 }, { timestamps: true });
 
 export default mongoose.model('Barber', barberSchema);
